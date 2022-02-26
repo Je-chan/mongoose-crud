dfeba19278cbe35d8415efc38503b5559785ff8a
박예찬 <jkyyc3@gmail.com>
2022-02-27 01:41:37 +0900

    Fix : Cors Origin Error

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,15 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div></div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import axios from 'axios'
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  setup() {
+    const getArticle = async () => {
+      const {data} = await axios.get('/read')
+      console.log(data)
+    }
+    
+    getArticle()
   }
 }
 </script>
--- server/index.js
+++ server/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
-
+const cors = require('cors');
 const app = express();
 const { Article } = require('./api');
 const PORT = 9000;
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // req.body - post 에서 들어온 payload 를 받을 수 있게 된다.
