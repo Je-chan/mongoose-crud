6f1ccb855ff755e7e4930b9b11d1279cca951bb7
박예찬 <jkyyc3@gmail.com>
2022-02-26 21:48:00 +0900

    CREATE : Mongoose Schema

---
+++ server/mongoose/model.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const schema = require('./schema');
+
+const db = mongoose.connection;
+
+const model = (() => {
+  db.on('error', console.error);
+  db.on('open', () => {
+    console.log('Connecting mongoDB');
+  });
+
+  // Atlas mongoDB cluster 와 연결
+  mongoose.connect(
+    `mongodb+srv://${process.env.MONGOOSE_ID}:${process.env.MONGOOSE_DB_PASSWORD}@mongodb-crud.g6uxj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+  );
+
+  // 스키마 연결
+  const model = {};
+  // 스키마는 여러가지가 있을 수 있다.
+  // 여러 개가 있어서 루프를 돌 수 있으므로, 일단 한 개밖에 없지만 for 로 돌려서 일반적인 경우를 확인
+  for (let key in schema) {
+    model[key] = mongoose.model(key, schema[key]);
+  }
+
+  return model;
+})();
+
+module.exports = model;
---
+++ server/mongoose/schema/index.js
@@ -0,0 +1,5 @@
+const Article = require('./artice');
+
+module.exports = {
+  Article,
+};
